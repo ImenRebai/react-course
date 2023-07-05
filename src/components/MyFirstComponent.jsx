e17f11b9279cb35d52f7667be23007ac121d9c93
Imen rebai <imenrb.info@gmail.com>
2023-07-05 18:36:12 +0100

    Adding classes to JSX element

--- src/components/MyFirstComponent.jsx
+++ src/components/MyFirstComponent.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./FirstComponentStyle.css"
 
 export default function MyFirstComponent() {
  
@@ -20,10 +21,11 @@ const elemStyle ={
     fontSize: "20px", 
     boxShadow: "0px 20px 10px"
 }}> {person.name}  </h1>
-      <h3 style = {{backgroundColor: "green"}}>{person.email}</h3>
-      <h2 style={elemStyle}>Hi</h2>
+      <h3 className={'active blBg'}>{person.email}</h3>
+      <h2 className={'active'}style={elemStyle}>Hi</h2>
       <button onClick={saySalem}>Click me</button>
     </div>
+    <h1 className={person.name=="Pingu coder" ? "blBg": "greenBg"}>Check Name</h1>
     </>
   )
 }
